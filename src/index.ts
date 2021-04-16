a8575c0f1f099196437e5f51b888bd600fa062be
vjdv <vjdv@outlook.com>
2021-04-16 14:24:21 -0500

    rename for better distinction

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import Col from "./components/col";
 import Form from "./formcontrols/form";
+import FormCol from "./components/form-col";
 import FormSelect from "./formcontrols/form-select";
+import FormRow from "./components/form-row";
 import FormTextInput from "./formcontrols/form-text-input";
 import FormValidation from "./formcontrols/form-validation";
 import Label from "./components/label";
-import Row from "./components/row";
 import Select from "./controls/select";
 import TextInput from "./controls/text-input";
 import useFormContext from "./hooks/useformcontext";
 import useFormState from "./hooks/useformstate";
 import useValue from "./hooks/usevalue";
 
-export { Col, Form, FormSelect, FormTextInput, FormValidation, Label, Row, Select, TextInput, useFormContext, useFormState, useValue };
+export { Form, FormCol, FormSelect, FormRow, FormTextInput, FormValidation, Label, Select, TextInput, useFormContext, useFormState, useValue };
